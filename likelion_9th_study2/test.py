e744529bfaa12a5105004a76c9c8ffc18e4f8e8f
HD152513 <qag331@naver.com>
2021-01-29 23:06:30 +0900

    210129| 멋사 9기 운영진 스터디| 2주차 파이썬 ~문자형|

---
+++ likelion_9th_study2/test.py
@@ -0,0 +1,24 @@
+
+print(1+2)
+print("멋쟁이 사자처럼")
+
+number = 2
+# print(number + 1)
+
+name = input("이름을 입력하세요")
+
+# print(name)
+
+# num1 = input("첫번쨰 숫자를 입력하세요")
+# num2 = input("두번쨰 숫자를 입력하세요")
+# print(num1 + num2)
+
+# num1 = int(input("첫번쨰 숫자를 입력하세요"))
+# num2 = int(input("두번쨰 숫자를 입력하세요"))
+# print(num1 + num2)
+
+#숫자형(int)
+num1 = int(input("num1 입력: "))
+num2 = float(input("num2 입력: "))
+print(num1+num2)
+#실수형(float)
