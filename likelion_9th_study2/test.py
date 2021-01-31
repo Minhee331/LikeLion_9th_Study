03625b43e36cba7abf870ee87a77c8b57d8c6ae3
HD152513 <qag331@naver.com>
2021-01-31 23:30:27 +0900

    210131| 멋사운영진스터디| 2주차 문자열 실습, 내장함수

--- likelion_9th_study2/test.py
+++ likelion_9th_study2/test.py
@@ -1,11 +1,11 @@
 
-print(1+2)
-print("멋쟁이 사자처럼")
+# print(1+2)
+# print("멋쟁이 사자처럼")
 
-number = 2
+# number = 2
 # print(number + 1)
 
-name = input("이름을 입력하세요")
+# name = input("이름을 입력하세요")
 
 # print(name)
 
@@ -18,7 +18,31 @@ name = input("이름을 입력하세요")
 # print(num1 + num2)
 
 #숫자형(int)
-num1 = int(input("num1 입력: "))
-num2 = float(input("num2 입력: "))
-print(num1+num2)
+# num1 = int(input("num1 입력: "))
+# num2 = float(input("num2 입력: "))
+# print(num1+num2)
 #실수형(float)
+
+# 문자열(내장 함수)
+
+#덧셈
+str = "멋쟁이사자처럼"
+str2 = "2주차 스터디 실습 중입니다. "
+print(str*3)
+print(str[0])
+
+#[x:y] --> x번째 인덱스부터 y인덱스 전까지
+print(str[0:4])
+
+#문자열의 길이: len()
+print(len(str))
+
+#문자열 내에서 특정 문자의 등장 횟수: 문자열변수.count('특정문자')
+print(str.count('멋'))
+
+#문자열을 (특정 기준으로) 나누기: 문자열 변수.split()
+print(str2.split()) #공백 기준으로 나눔
+
+#특정 문자 인덱스 찾기: find('문자') , index('문자')
+print(str.find('사'))
+print(str.index('사'))
